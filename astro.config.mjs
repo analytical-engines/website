b89d4425dcf27d42effaaea88b2ff7d54d6d03b9
nekonya <nora.nekosan+github@gmail.com>
2024-05-26 15:17:46 +0900

    modify base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://analytical-engines.com',
-    base: '/ja',
+    base: '/',
 	integrations: [
 		starlight({
             defaultLocale: 'ja',
