b6f2f4427be6080621d6e79d50cdc9e79912141a
nekonya <nora.nekosan+github@gmail.com>
2024-05-26 14:43:31 +0900

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,45 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://analytical-engines.com',
+    base: '/ja',
+	integrations: [
+		starlight({
+            defaultLocale: 'ja',
+			title: {
+                ja: '解析機関',
+                en: 'Analytical Engines',
+            },
+            locales: {
+                ja: {
+                    label: '日本語',
+                    lang: 'ja',
+                    //title: '解析機関',
+                },
+                en: {
+                    label: 'English',
+                    lang: 'en',
+                    //title: 'Analytical Engines',
+                },
+            },
+			social: {
+				github: 'https://github.com/analytical-engines',
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', link: '/guides/example/' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
